ecd758036372fcb07c531b7923328ff7de1e7782
chmodder <hotmailgateway@gmail.com>
2013-11-19 11:27:05 +0100

    Created Macro, NetUserControl and new user
    Finished day 3

---
+++ umbraco-tut/umbraco-tut/usercontrols/WebUserControl1.ascx
@@ -0,0 +1,5 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="WebUserControl1.ascx.cs" Inherits="umbraco_tut.usercontrols.WebUserControl1" %>
+<asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+<asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Button" />
+<asp:Literal ID="Literal1" runat="server"></asp:Literal>
+
---
+++ umbraco-tut/umbraco-tut/usercontrols/WebUserControl1.ascx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace umbraco_tut.usercontrols
+{
+    public partial class WebUserControl1 : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Literal1.Text = "Du er nu kommet ind på siden og har ikke aktiveret noget endnu";
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Literal1.Text = "du har skrevet i tekstboksen:" + TextBox1.Text;
+        }
+    }
+}
